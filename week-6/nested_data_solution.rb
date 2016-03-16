4921da76a633db9007a88919d6963730f9e3eaea
Nova840 <adamzmudzinski2@gmail.com>
2016-03-16 15:15:07 -0400

    6.5: Add reflection to nested_data_solution.rb

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -10,7 +10,7 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 #p array[2][2][3][1]
 #p array[2]
 #p array[1]
-#p array[1][1][2][0]
+p array[1][1][2][0]
 
 # ============================================================
 
@@ -24,7 +24,7 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 
 #p hash[outer:][inner:]["almost"][3]
 #p hash[:outer]
-#p hash[:outer][:inner]["almost"][3]
+p hash[:outer][:inner]["almost"][3]
 
 # ============================================================
 
@@ -59,7 +59,7 @@ end
 
 
 add5! number_array
-#p number_array
+p number_array
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
@@ -77,3 +77,21 @@ end
 
 addLy! startup_names
 p startup_names
+
+=begin
+
+Reflection:
+
+What are some general rules you can apply to nested arrays?
+
+  Nested arrays are arrays with one or more arrays inside of the array.
+
+What are some ways you can iterate over nested arrays?
+
+  You can use nested loops if you know how many dimensions the array has of recursion if you don't.
+
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+
+  We didn't use any new methods. I couldn't find any that would better do what I wanted than the ones I already knew.
+
+=end
